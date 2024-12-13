5800dc36353ad85dfab849686c1f2c37a50a37b4
unknown <ttv.lipey@gmail.com>
2024-12-13 15:56:13 -0300

    Update 3.5

--- helper.sh
+++ helper.sh
@@ -5,6 +5,11 @@ parse_ports() {
     local config_file="$HOME/vps.config"
     local port_args=""
     
+    # Create config file if it doesn't exist
+    if [ ! -f "$config_file" ]; then
+        touch "$config_file"
+    fi
+    
     while read -r line; do
         case "$line" in
             internalip=*) ;;
@@ -31,7 +36,6 @@ exec_proot() {
     -0 -w "${HOME}" \
     -b /dev -b /sys -b /proc -b /etc/resolv.conf \
     $port_args \
-    --kill-on-exit \
     /bin/sh -c "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin exec /run.sh"
 }
 
